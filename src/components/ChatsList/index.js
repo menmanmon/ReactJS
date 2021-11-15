6c8152346509dc915d6ffba43775030835124c82
manmanmon <burgoch-roman9a@mail.ru>
2021-11-15 18:09:23 +0300

    fx

--- src/components/ChatsList/index.js
+++ src/components/ChatsList/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import CustomButton from '@mui/material/Button';
+import './ChatsList.css';
 
 export const ChatsList = ({ onAddChatClick }) => {
     const [chats, setChats] = useState([
@@ -17,19 +18,22 @@ export const ChatsList = ({ onAddChatClick }) => {
             id: 'chat3',
         },
     ]);
-    // useEffect(() => {
-
-    // }, []);
     const addNewChat = () => {
         let numberOfChats = Object.keys(chats).length
         setChats((chats.push({ name: 'chat ' + (numberOfChats + 1), id: 'chat' + (numberOfChats + 1) })))
         console.log(chats);
     }
     console.log(chats);
-    const chat = chats.map((chat) => <Link to={`/chats/${chat.id}`} key={chat.id}>{chat.name}</Link>)
-
+    const chat = chats.map((chat, index) =>
+        <div className="chatName">
+            <Link to={`/chats/${chat.id}`} key={chat.id}>{chat.name}</Link>
+            <button onClick={() => deleteThisChat(index)}>Delete</button>
+        </div>)
+    const deleteThisChat = (index) => {
+        setChats([...chats.slice(0, index), ...chats.slice(index + 1)]);
+    }
     return (
-        <div>
+        <div className="chatsList">
             <h3>Chats List</h3>
             <CustomButton onClick={onAddChatClick, addNewChat} >Add chat</CustomButton>
             <ul className="chatsList">
