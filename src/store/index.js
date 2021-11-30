ceacf85888a22425692f8a092532229a35722873
manmanmon <burgoch-roman9a@mail.ru>
2021-11-30 16:36:45 +0300

    В процессе

--- src/store/index.js
+++ src/store/index.js
@@ -5,17 +5,20 @@ import { chatsReduser } from './chats/reduser';
 import { messagesReduser } from './messages/reduser';
 import { profileReducer } from './profile/reducer';
 import { persistStore, persistReducer } from 'redux-persist';
+import { articlesReduser } from './articles/reducer';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const config = {
     key: 'gbMessages',
     storage,
+    blacklist: ['profile', 'articles']
 }
 
 const presistedReducer = persistReducer(config, combineReducers({
     profile: profileReducer,
     chats: chatsReduser,
     messages: messagesReduser,
+    articles: articlesReduser,
 }))
 
 export const store = createStore(presistedReducer, composeEnhancers(applyMiddleware(thunk)));
--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,3 +1,5 @@
+import { REQURST_STATUS } from "../utils/constants"
+
 export function getChatsList(state) {
     return state.chats
 }
@@ -8,4 +10,16 @@ export function getMessagesList(state) {
 
 export function getPrifile(state) {
     return state.profile
+}
+
+export function selectArticlesList(state) {
+    return state.articles.articlesList
+}
+
+export function selectArticlesLoading(state) {
+    return state.articles.request.status === REQURST_STATUS.LOADING 
+}
+
+export function selectArticlesError(state) {
+    return state.articles.request.error
 }
